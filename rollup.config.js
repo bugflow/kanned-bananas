bd275f892025a94bd26d8b1691caeba676059b80
David Ormerod <dormerod@users.noreply.github.com>
2020-03-22 14:22:18 +1100

    Add Luxon (date wrapper library)

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import babel from "rollup-plugin-babel";
 import pkg from "./package.json";
 
-const external = ["axios", "dotenv", "fs", "graphql-request", "util"];
+const external = ["axios", "dotenv", "fs", "graphql-request", "luxon", "util"];
 const plugins = [
   resolve({
     mainFields: ["module", "main"],
