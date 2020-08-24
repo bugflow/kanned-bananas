42adfbab754f8fd5be47cdada788bb2f194ff73b
David Ormerod <dormerod@users.noreply.github.com>
2020-08-24 20:55:03 +1000

    Only request Zenhub events for relevant tickets

--- src/data/Data.js
+++ src/data/Data.js
@@ -18,8 +18,8 @@ class Data {
       this.zenhubIssues = this.cache.data.zenhubIssues;
       this.zenhubBoards = this.cache.data.zenhubBoards;
     } else {
-      await this.loadGithubData(project);
-      await this.loadZenhubData(project);
+      await this.loadGithubData({ project });
+      await this.loadZenhubData({ project, time });
 
       const json = JSON.stringify({
         lastUpdated: DateTime.utc().toString(),
@@ -34,9 +34,10 @@ class Data {
     return this;
   }
 
-  async loadGithubData(project) {
+  async loadGithubData({ project }) {
     for await (const repo of project.repos) {
       try {
+        console.log(`Requesting Github issues for ${repo.name}...`);
         const issues = await this.api.getGithubIssues(repo.owner, repo.name);
 
         // save arrays of closed issues for adding to the Zenhub board later
@@ -44,6 +45,7 @@ class Data {
           .filter(issue => issue.node.closed)
           .map(issue => ({
             issue_number: issue.node.number,
+            closedAt: issue.node.closedAt,
             is_epic: null, // TODO: consider adding epic status if useful
             position: null,
           }));
@@ -57,9 +59,13 @@ class Data {
     }
   }
 
-  async loadZenhubData(project) {
+  async loadZenhubData({ project, time }) {
+    // TODO (dormerod): make ignored array configurable
+    const ignoredColumns = ["Admin"];
+
     for await (const repo of project.repos) {
       try {
+        console.log(`Requesting Zenhub board for ${repo.name}...`);
         const board = await this.api.getZenhubBoard({ project, repo });
 
         // add another column for closed isses (not included by default)
@@ -79,48 +85,57 @@ class Data {
         // add Zenhub issue events and other data to issues
         for await (const column of board.pipelines) {
           for await (const issue of column.issues) {
-            await sleep(600); // Zenhub API rate limit is 100 requests per min
+            // skip tickets in the ignored columns
+            if (ignoredColumns.includes(column.name)) break;
 
-            issue.column = column.name;
-            issue.repoName = repo.name;
-            issue.repoID = repo.zenhubID;
-            issue.events = await this.cache.ZenhubEvents({
-              repo,
-              issue,
-            });
-
-            // find the matching issue from Github, to get the title etc
-            const foundRepo = this.githubIssues.find(
-              searchedRepo => searchedRepo.name === repo.name,
-            );
-
-            let foundIssue = null;
-            if (foundRepo) {
-              foundIssue = foundRepo.issues.find(
-                searched => searched.node.number === issue.issue_number,
+            // only care about closed tickets if they're in the reporting range
+            if (column.id !== "closed" || time.isBeforeStart(issue.closedAt)) {
+              issue.column = column.name;
+              issue.repoName = repo.name;
+              issue.repoID = repo.zenhubID;
+              console.log(
+                `Requesting events from ${column.name}: ${repo.name}#${issue.issue_number}...`,
               );
-            }
+              issue.events = await this.cache.ZenhubEvents({
+                repo,
+                issue,
+              });
 
-            if (foundIssue) {
-              // if we found the Github issue, add issue close events and title
-              issue.title = foundIssue.node.title;
-              if (foundIssue.node.closed) {
-                issue.events.unshift({
-                  user_id: null,
-                  type: "transferIssue",
-                  created_at: foundIssue.node.closedAt,
-                  from_pipeline: {
-                    name: null,
-                  },
-                  to_pipeline: {
-                    name: "Closed",
-                  },
-                  workspace_id: null,
-                });
+              // find the matching issue from Github, to get the title etc
+              const foundRepo = this.githubIssues.find(
+                searchedRepo => searchedRepo.name === repo.name,
+              );
+
+              let foundIssue = null;
+              if (foundRepo) {
+                foundIssue = foundRepo.issues.find(
+                  searched => searched.node.number === issue.issue_number,
+                );
               }
-            }
 
-            this.zenhubIssues.push(issue);
+              if (foundIssue) {
+                // if we found the Github issue, add issue close events and title
+                issue.title = foundIssue.node.title;
+                if (foundIssue.node.closed) {
+                  issue.events.push({
+                    user_id: null,
+                    type: "transferIssue",
+                    created_at: foundIssue.node.closedAt,
+                    from_pipeline: {
+                      name: null,
+                    },
+                    to_pipeline: {
+                      name: "Closed",
+                    },
+                    workspace_id: null,
+                  });
+                }
+              }
+
+              this.zenhubIssues.push(issue);
+
+              await sleep(600); // Zenhub API rate limit is 100 requests per min
+            }
           }
         }
       } catch (e) {
