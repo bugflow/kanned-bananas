a6d1b9ecb5e413f4413d729afee05839e2ade133
David Ormerod <dormerod@users.noreply.github.com>
2021-02-21 15:03:44 +1100

    Add 'UX' to list of reported labels

--- src/reports/format/labels.js
+++ src/reports/format/labels.js
@@ -2,6 +2,7 @@ const keyLabels = [
   { label: /^goal$/i, description: "Goal", format: "bold" },
   { label: /^epic$/i, description: "Epic" },
   { label: /^bug$/i, description: "Bug" },
+  { label: /^ux$/i, description: "UX" },
   { label: /^request$/i, description: "Request" },
   { label: /^feature$/i, description: "Feature" },
   { label: /^enhancement$/i, description: "Enhancement" },
