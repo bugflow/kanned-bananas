613a2b16ae352025c3e331ca81e861b7e860402a
David Ormerod <dormerod@users.noreply.github.com>
2021-02-28 16:52:42 +1100

    Support options at top level of config file

--- src/config/get-config.js
+++ src/config/get-config.js
@@ -4,11 +4,12 @@ import projectDefaults from "./project-defaults";
 function getConfig(configFile, cliConfig = {}) {
   const config = readConfig(configFile);
 
-  // apply defaults, first CLI, then config file and finally project defaults
+  // apply config: CLI > project file config > top level file config > defaults
   config.projects = config.projects.map(project => {
     const projectData = {};
     Object.keys(projectDefaults).forEach(key => {
-      projectData[key] = cliConfig[key] || project[key] || projectDefaults[key];
+      projectData[key] =
+        cliConfig[key] || project[key] || config[key] || projectDefaults[key];
     });
 
     return projectData;
--- src/config/get-config.test.js
+++ src/config/get-config.test.js
@@ -6,17 +6,21 @@ const config = getConfig(testConfigFile, cliConfig);
 const project = config.projects[0];
 
 describe("Combine config file with command line settings and defaults", () => {
-  it("should use defaults if the config doesn't specify a value", () => {
-    expect(project.period).toBe("week");
+  it("should override other values with any values from the CLI args", () => {
+    expect(project.title).toBe("New Title!");
+  });
+  it("should use project level config if CLI doesn't override it", () => {
     expect(project.fromTimestamp).toBe("19000101T12:00:00");
+  });
+  it("should check top level of config file there's nothing project specific", () => {
+    expect(project.period).toBe("fortnight");
+  });
+  it("should use defaults if no other value is specified", () => {
     expect(project.excludedLabels).toStrictEqual([
       "/^duplicate$/",
       "/^exclude$/",
       "/^ignore$/",
       "/^overtaken$/",
     ]);
-  });
-  it("should override other values with any values from the CLI args", () => {
-    expect(project.title).toBe("New Title!");
   });
 });
