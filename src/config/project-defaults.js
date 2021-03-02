b143785d6dd0bfca3f8afa2d2deb2e67a02ff08a
David Ormerod <dormerod@users.noreply.github.com>
2021-03-02 19:52:43 +1100

    Update comments on default config

--- src/config/project-defaults.js
+++ src/config/project-defaults.js
@@ -1,20 +1,20 @@
 // all legal properties should be defined here, because they get iterated
 const projectDefaults = {
-  title: null, // this is what appears on the repot (h1)
-  subtitle: null, // h2 subheading
+  title: null, // this appears at the top of the report as h1
+  subtitle: null, // this is the h2 subheading
   reportType: "delivery", // one of "milestones", "stocks" and "delivery"
-  period: "week", // one of "day", "week", "month", "fortnight", "quarter", "year"
+  period: "week", // "day", "week", "month", "fortnight", "quarter" or "year"
   fromTimestamp: null, // ISO8601
   toTimestamp: null,
-  overdelivery: false, // if something is not in the sprint backlog, report it as "over-delivery"
-  includedLabels: [], // regex expression string, not .js regex
+  overdelivery: false, // report overdelivery if tickets skip the sprint backlog
+  includedLabels: [], // regex string, not regex type (".*" not /.*/) see below
   excludedLabels: [
     "/^duplicate$/",
     "/^exclude$/",
     "/^ignore$/",
     "/^overtaken$/",
   ],
-  includedMilestones: [], // GitHub milestone name
+  includedMilestones: [], // GitHub milestone name as a string
   excludedMilestones: [],
   includedColumns: [], // TODO (dormerod): support filtering + excluding here
   zenhubWorkspaceID: null, // get this UUID from ZenHub URL, blech
