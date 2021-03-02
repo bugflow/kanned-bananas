9573b871ed8897460c52442332cb91e1c8e982ef
Chris Gough <christopher.d.gough@gmail.com>
2021-03-02 13:23:32 +1100

    comment the default config file

--- src/config/project-defaults.js
+++ src/config/project-defaults.js
@@ -1,23 +1,23 @@
 // all legal properties should be defined here, because they get iterated
 const projectDefaults = {
-  title: null,
-  subtitle: null,
-  reportType: "delivery",
-  period: "week",
-  fromTimestamp: null,
+  title: null, // this is what appears on the repot (h1)
+  subtitle: null, // h2 subheading
+  reportType: "delivery", // one of "milestones", "stocks" and "delivery"
+  period: "week", // one of "day", "week", "month", "fortnight", "quarter", "year"
+  fromTimestamp: null, // ISO8601
   toTimestamp: null,
-  overdelivery: false,
-  includedLabels: [],
+  overdelivery: false, // if something is not in the sprint backlog, report it as "over-delivery"
+  includedLabels: [], // regex expression string, not .js regex
   excludedLabels: [
     "/^duplicate$/",
     "/^exclude$/",
     "/^ignore$/",
     "/^overtaken$/",
   ],
-  includedMilestones: [],
+  includedMilestones: [], // GitHub milestone name
   excludedMilestones: [],
   includedColumns: [], // TODO (dormerod): support filtering + excluding here
-  zenhubWorkspaceID: null,
+  zenhubWorkspaceID: null, // get this UUID from ZenHub URL, blech
   repos: null,
 };
 
